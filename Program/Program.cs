59c0f99c64666c97eb78c35508b1925e1aa69c1d
ValeraZib <valerazib@gmail.com>
2023-07-25 15:26:33 +0300

    feat: write the core of method, which create and fill new array

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,3 +8,22 @@
 }
 string [] array = new string [6] {"if", "slame", "tap", "global", "supreme", "OK"};
 PrintArray (array);
+
+void CreateFillAndPrintNewArray (string [] arr)
+{   int IndArr2 = 0;
+    string [] arr2 = new string [6];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length<=3)
+        {
+            arr2[IndArr2] = arr[i];
+            IndArr2++;
+        }
+
+        
+    }
+    PrintArray(arr2);
+}
+Console.WriteLine("");
+CreateFillAndPrintNewArray(array);
