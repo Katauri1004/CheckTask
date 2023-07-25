cd6a14c77fea884d8b4abca267f08d8cff6d9eed
ValeraZib <valerazib@gmail.com>
2023-07-25 12:48:06 +0300

    feat: write method for print array

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,10 @@
-﻿string [] array = new string [6] {"if", "slame", "tap", "global", "supreme", "OK"};
+﻿void PrintArray (string [] arr)
+{
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        Console.Write ($"[{arr [i]}]  ");
+    
+    }
+}
+string [] array = new string [6] {"if", "slame", "tap", "global", "supreme", "OK"};
+PrintArray (array);
